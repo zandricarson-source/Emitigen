88832df232fc1bb76506fa38ed253369cebb0e36
zandricarson-source <zandricarson@gmail.com>
2026-01-16 14:59:44 -0500

    added dev comments to format how this will all function

--- Emitigen.py
+++ Emitigen.py
@@ -1,6 +1,18 @@
 """
 SETUP:
-Use: wavelengths, intensities, table = get_element_data('H', 'I')
+Use: generates a sound frequency and amplitude scaled from the wavelength and relative intensity of an element
+1. NIST ASD data (input element='H') ion stage default I, wavelength range default (4000, 7000)
+    1.1 parse table to get wavelength (Å) and RI (excluding where RI is N/A)
+    1.2 return Å, RI table
+2. new table with sound data
+    2.1 convert Å to Hz scaled from 4000-7000 to 20.0, 20000.0
+    2.2 convert RI to floating point amplitude
+        2.2.1 scale RI range from 0 to 10000 to range of 0 to 1 
+    2.3 return Hz, Amp table
+3. convert Å to RGB
+    3.1 divide Å by 10 to get nm
+    3.2 use wavelength_to_rgb
+
 """
 
 from astroquery.nist import Nist
@@ -8,6 +20,63 @@ import astropy.units as u
 import numpy as np
 
 VISIBLE_RANGE = (4000, 7000)
+
+def get_element_table(element='H'):
+    """
+    Docstring for get_element_table
+    
+    :param element: Element key, default is Hydrogen
+    :return: a table of A wavelength, RE relative intensity, Hz sound frequency, 
+    """
+
+    return None
+
+# from github: https://gist.github.com/error454/65d7f392e1acd4a782fc
+def wavelength_to_rgb(wavelength, gamma=0.8):
+
+    '''This converts a given wavelength of light to an 
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+
+    wavelength = float(wavelength)
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    R *= 255
+    G *= 255
+    B *= 255
+    return (int(R), int(G), int(B))
 
 def get_element_data(element, ion_stage='I', wavelength_range=None):
     """
@@ -155,6 +224,16 @@ def get_strongest_lines(wavelengths, intensities, n_strongest=10):
     sorted_idx = np.argsort(valid_int)[::-1]  # Descending order
     
     return valid_wl[sorted_idx[:n_strongest]], valid_int[sorted_idx[:n_strongest]]
+
+def A_to_hz_log(A):
+    A = max(4000, min(7000, A))
+
+    wl_min, wl_max = 4000, 7000
+    f_min, f_max = 20.0, 20000.0
+
+    x = (wl_max - A) / (wl_max - wl_min)
+
+    return f_min * (f_max / f_min) ** x
 
 
 # Example usage
